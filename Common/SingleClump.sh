e80446814db0dc0ade6f66f4b5cadcd4e2244bf9
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2023-08-05 03:07:45 +0800

    Update SingleClump.sh

--- Common/SingleClump.sh
+++ Common/SingleClump.sh
@@ -3,10 +3,10 @@
 for lines in $(cat PhenoList);do
 plink --bfile /share/home1/Vinceyang/dyt_data/GWAS/data/1000G_Phase_3/EUR/EUR.1kg.phase3.v5a \
 --clump ${lines}_Significant.csv \
---clump-p1 1e-8 \
---clump-p2 0.01 \
+--clump-p1 5e-8 \
+--clump-p2 1e-6 \
 --clump-r2 0.1 \
---clump-kb 10000 \
+--clump-kb 500 \
 --clump-field P \
 --indep-pairwise 500 50 0.2 \
 --clump-snp-field ID \
